08a6cc04d72433338ea21b789dc7af487c29dcc9
LysBohdan <bohdan.lys.ki.2020@lpnu.ua>
2022-08-30 18:36:05 +0300

    Add code to Lab1

--- KI34.Lys.Lab1/src/Main.java
+++ KI34.Lys.Lab1/src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         String message = new String();
         message = "Lys Bohdan KI-34";
-
         System.out.println(message);
     }
 }
